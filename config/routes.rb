6a81360a1b7d78db8d539e155c75818d4c3f0ac0
EC2 Default User <ec2-user@ip-172-31-43-74.ap-southeast-1.compute.internal>
2018-04-23 12:37:13 +0000

    View file was changed.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get  'translations'     => 'translations#index'
   get  'translations/new' => 'translations#new'
+  root 'translations#new'
   post 'translations'     => 'translations#create'
 end
