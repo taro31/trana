52ee9e2f4dfb9ef657dda4ed4fed7d81da0e48aa
EC2 Default User <ec2-user@ip-172-31-43-74.ap-southeast-1.compute.internal>
2018-06-27 10:26:02 +0000

    mini fix

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get  'translations/new' => 'translations#new'
   root 'translations#new'
   post 'translations'     => 'translations#create'
+  get  'translations/new/upimg' => 'translations#upimg' #場所間違ってるかも
 end
